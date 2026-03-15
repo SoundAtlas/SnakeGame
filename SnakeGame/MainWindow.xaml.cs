fa199484200d17af2c8eb2431777822e0c34e234
Andreas Hemmer <andreas.hemmer@hotmail.com>
2026-03-15 14:48:00 +0100

    Add gamearea to UI

--- SnakeGame/MainWindow.xaml.cs
+++ SnakeGame/MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace SnakeGame
 {
@@ -16,6 +7,9 @@ namespace SnakeGame
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private readonly int rows = 15, cols = 15;
+
         public MainWindow()
         {
             InitializeComponent();
