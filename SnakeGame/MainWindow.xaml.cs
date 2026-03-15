fca9438d4d39d2633ea2384c3e700118c3903758
Andreas Hemmer <andreas.hemmer@hotmail.com>
2026-03-15 15:11:52 +0100

    Add DrawGrid() method

--- SnakeGame/MainWindow.xaml.cs
+++ SnakeGame/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 
 namespace SnakeGame
@@ -9,14 +10,23 @@ namespace SnakeGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Dictionary<GridValue, ImageSource> gridValToImage = new()
+        {
+            { GridValue.Empty, Images.Empty },
+            { GridValue.Snake, Images.Body },
+            { GridValue.Food, Images.Food  },
+        };
 
         private readonly int rows = 15, cols = 15;
         private readonly Image[,] gridImages;
+
+        private GameState gameState;
 
         public MainWindow()
         {
             InitializeComponent();
             gridImages = SetupGrid();
+            gameState = new GameState(rows, cols);
         }
 
         private Image[,] SetupGrid()
@@ -40,6 +50,21 @@ namespace SnakeGame
             }
 
             return gridImages;
+        }
+
+
+
+
+        private void DrawGrid()
+        {
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    GridValue gridValue = gameState.Grid[r, c];
+                    gridImages[r, c].Source = gridValToImage[gridValue];
+                }
+            }
         }
     }
 }
