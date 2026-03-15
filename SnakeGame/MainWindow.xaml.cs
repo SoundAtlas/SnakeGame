e572841103edf5f1cf5996b30a77ab344c3c0bcf
Andreas Hemmer <andreas.hemmer@hotmail.com>
2026-03-15 16:15:26 +0100

    Add pause before game start

--- SnakeGame/MainWindow.xaml.cs
+++ SnakeGame/MainWindow.xaml.cs
@@ -20,8 +20,8 @@ namespace SnakeGame
 
         private readonly int rows = 15, cols = 15;
         private readonly Image[,] gridImages;
-
         private GameState gameState;
+        private bool gameRunning;
 
         public MainWindow()
         {
@@ -30,10 +30,26 @@ namespace SnakeGame
             gameState = new GameState(rows, cols);
         }
 
-        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        private async Task RunGame()
         {
             Draw();
+            Overlay.Visibility = Visibility.Hidden;
             await GameLoop();
+        }
+
+        private async void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (Overlay.Visibility == Visibility.Visible)
+            {
+                e.Handled = true;
+            }
+
+            if (!gameRunning)
+            {
+                gameRunning = true;
+                await RunGame();
+                gameRunning = false;
+            }
         }
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
